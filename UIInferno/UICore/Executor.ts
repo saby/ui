cefb959c72b380a56b67b056dff8110721958907
va.ten <va.ten@tensor.ru>
2021-07-19 10:07:51 +0300

    https://online.sbis.ru/opendoc.html?guid=46a3a9eb-25b5-43b3-9a67-9a880789a91f

--- UIInferno/UICore/Executor.ts
+++ UIInferno/UICore/Executor.ts
@@ -7,3 +7,11 @@ export {
     portalTagName,
     controlNode
 } from './_executor/_Utils/Vdom';
+
+/**
+ * для совместимого генератора
+ */
+export * as Decorate from './_executor/_Expressions/Decorate';
+export * as MarkupUtils from './_executor/_Markup/Utils';
+export { ResolveControlName } from './_executor/_Markup/ResolveControlName';
+export { Generator as GeneratorBase } from './_executor/_Markup/Generator';
