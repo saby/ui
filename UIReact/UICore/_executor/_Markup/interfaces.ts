5624be01c7bc1a84874e085a412a92f167ebd915
aa.shipin <aa.shipin@tensor.ru>
2021-08-12 14:01:37 +0300

    https://online.sbis.ru/opendoc.html?guid=ca3fd30e-1453-43fd-98e1-98f3a240f561

--- UIReact/UICore/_executor/_Markup/interfaces.ts
+++ UIReact/UICore/_executor/_Markup/interfaces.ts
@@ -15,7 +15,7 @@ import { IWasabyEvent } from 'UICommon/Events';
 export interface IGeneratorAttrs {
     attributes: Attr.IAttributes;
     events: Record<string, IWasabyEvent[]>;
-    _$parent: Control;
+    _$parent?: Control;
 }
 
 export interface IControlConfig {
