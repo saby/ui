fd4322e17344dfe4b5fe11c99a5026c24341a891
va.ten <va.ten@tensor.ru>
2021-08-23 17:32:54 +0300

    https://online.sbis.ru/opendoc.html?guid=674674d7-33f0-44f6-9b5d-bde05b3514ef

---
+++ UICommon/_utils/IsSvgTarget.ts
@@ -0,0 +1,29 @@
+import {detection} from 'Env/Env';
+
+/**
+ * Модуль, в котором описана функция <b>isSvgTarget(elem)</b>.
+ *
+ * Метод выполняет в IE для переданного элемента и находит родителя для SVG элемента:
+ *
+ * <h2>Параметры функции</h2>
+ * <ul>
+ *     <li><b>elem</b> проверяемый на видимость элемент.</li>
+ * </ul>
+ *
+ * <h2>Возвращает</h2>
+ * {EventTarget} Элемент.
+ *
+ * @class UICommon/_utils/IsSvgTarget
+ * @public
+ * @author Тэн В.А.
+ */
+
+export default function isSvgTarget(element: EventTarget & {correspondingUseElement?: SVGUseElement}): EventTarget {
+    if (detection.isIE) {
+        while (element.correspondingUseElement) {
+            element = element.correspondingUseElement.parentNode;
+        }
+        return element;
+    }
+    return element;
+}
